8ebafdbcab8f638a507a394f51c4b5c9f3e3f8a9
lkrra <lkr.rafa@gmail.com>
2023-08-24 23:06:28 +0200

    Moved to NestJs 10

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
-import { seedKepwareDB } from './kepware/seed';
-import { seedMachinesDB } from './machines/seed';
+// import { seedKepwareDB } from './kepware/seed';
+// import { seedMachinesDB } from './machines/seed';
 
 export const seed = async () => {
-  await Promise.all([seedMachinesDB(), seedKepwareDB()]);
+  // await Promise.all([seedMachinesDB(), seedKepwareDB()]);
 };
 
 seed();
