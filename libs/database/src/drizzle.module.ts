62ac152b2c3d59f5e56ae40637b81c4cc63614ba
lkrra <lkr.rafa@gmail.com>
2023-08-27 20:52:44 +0200

    Deleted excessive configuration

--- libs/database/src/drizzle.module.ts
+++ libs/database/src/drizzle.module.ts
@@ -4,28 +4,12 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import { ConfigService } from '@nestjs/config';
 
-import * as schema from '../../../apps/machines/src/database/schema';
-import { PG_CONNECTION } from '../../../apps/machines/src/constants';
-
 type DrizzleModuleOptions = {
   name: string;
   schema: any;
 };
 
-@Module({
-  providers: [
-    {
-      provide: PG_CONNECTION,
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const connectionString = configService.get<string>('DATABASE_URL');
-        const pool = postgres(connectionString);
-        return drizzle(pool, { schema });
-      },
-    },
-  ],
-  exports: [PG_CONNECTION],
-})
+@Module({})
 export class DrizzleModule {
   static register({ name, schema }: DrizzleModuleOptions): DynamicModule {
     return {
